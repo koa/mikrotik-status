2ae60e764a4072ad1f07446db18d16c8503015bf
akoenig <andreas.koenig@berg-turbenthal.ch>
2022-12-21 15:24:55 +0100

    include frontend build into binary

--- binary/build.rs
+++ binary/build.rs
@@ -1,5 +1,9 @@
 use static_files::resource_dir;
 
 fn main() -> std::io::Result<()> {
+    std::process::Command::new("trunk")
+        .current_dir("../frontend")
+        .args(["build", "--release"])
+        .spawn()?;
     resource_dir("../frontend/dist").build()
 }
