7be28775659734029e0bda6a5c3cbcc9624f824b
akoenig <andreas.koenig@berg-turbenthal.ch>
2022-12-21 15:33:27 +0100

    Revert "include frontend build into binary"
    This reverts commit 2ae60e764a4072ad1f07446db18d16c8503015bf.

--- binary/build.rs
+++ binary/build.rs
@@ -1,9 +1,5 @@
 use static_files::resource_dir;
 
 fn main() -> std::io::Result<()> {
-    std::process::Command::new("trunk")
-        .current_dir("../frontend")
-        .args(["build", "--release"])
-        .spawn()?;
     resource_dir("../frontend/dist").build()
 }
